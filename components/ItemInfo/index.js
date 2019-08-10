f3e686f86ff00755722fe1562210c96a055976b1
Maciej Kurek <maciejk77@gmail.com>
2019-08-10 14:10:05 +0100

    components structure added

---
+++ components/ItemInfo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ItemInfo = () => {
+  return <div>ItemInfo</div>;
+};
+
+export default ItemInfo;
--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import NavBar from './components/NavBar';
+import ItemInfo from './components/ItemInfo';
+import Graph from './components/Graph';
+import Footer from './components/Footer';
+
 const App = () => {
-  return <div>OE Test App</div>;
+  return (
+    <div>
+      <NavBar />
+      <ItemInfo />
+      <ItemInfo />
+      <Graph />
+      <Footer />
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
